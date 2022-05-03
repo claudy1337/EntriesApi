fa3c24502444dbc651105923eef052b24ed6b7bf
claudy1337 <45602451+claudy1337@users.noreply.github.com>
2022-05-03 11:07:54 +0300

    category fix

--- EntriesApi/EntriesApi/ViewModel/EntriesListPage.xaml.cs
+++ EntriesApi/EntriesApi/ViewModel/EntriesListPage.xaml.cs
@@ -23,18 +23,28 @@ namespace EntriesApi.ViewModel
             base.OnAppearing();
             Entries = await App.RequestManager.GetEntrieModels();
             lvEntries.ItemsSource = Entries;
+            PCCategory.ItemsSource = Entries;
         }
-
         private void sbSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
             var searchText = SBSearchAPI.Text.ToLower();
             lvEntries.ItemsSource = Entries.Where(en => en.API.ToLower().Contains(searchText) || en.Description.ToLower().Contains(searchText));
         }
-
         private async void lvEntries_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             
             
+        }
+        private void PCCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (PCCategory.SelectedIndex == -1)
+            {
+                DisplayAlert("error", "index -1", "ОK");
+            }
+            else
+            {
+                lvEntries.ItemsSource = Entries.Where(en => en.Category.Contains(PCCategory.SelectedItem()));
+            }
         }
     }
 }
