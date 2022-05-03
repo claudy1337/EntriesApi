b0ad1763d7fcbfceaad3cecd01a20d99e8349156
claudy1337 <45602451+claudy1337@users.noreply.github.com>
2022-05-03 10:13:08 +0300

    service folder add

--- EntriesApi/EntriesApi/App.xaml.cs
+++ EntriesApi/EntriesApi/App.xaml.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using EntriesApi.Model;
+using EntriesApi.Services;
 
 namespace EntriesApi
 {
     public partial class App : Application
     {
+        public static RequestManager RequestManager { get; private set; }
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            RequestManager = new RequestManager(new RestService());
+            MainPage = new NavigationPage(new EntriesListPage());
         }
 
         protected override void OnStart()
